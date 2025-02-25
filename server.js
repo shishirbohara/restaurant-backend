3bf12ff4e396d3a0d6d61da7433eebe195c5230e
shishirbohara <shishirbohara68@gmail.com>
2025-02-25 15:12:11 +0545

    db config

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const pool = require("./db");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+
+pool
+  .connect()
+  .then(() => {
+    console.log("DB connection success.");
+  })
+  .catch((err) => {
+    console.log("DB connection error.", err.stack);
+  })
+  .finally(() => {
+    pool.end();
+  });
+
 app.listen(PORT, () => {
-  console.log(`Server is running on : ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
