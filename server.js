9b93b4360751c56af06790fb45c95febb2a49ef6
shishirbohara <shishirbohara68@gmail.com>
2025-02-25 16:17:21 +0545

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const pool = require("./db");
+const cors = require("cors");
 const menuRoutes = require("./routes/menuRoutes");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/", menuRoutes);
 
 pool
